be9917df5558fd43d6737c079f4ba739f2ab2173
Mario <linguoqiang100@gmail.com>
2026-03-06 16:18:34 +0800

    first commit

---
+++ tigerflowTests/tigerflowTests.swift
@@ -0,0 +1,16 @@
+//
+//  tigerflowTests.swift
+//  tigerflowTests
+//
+//  Created by Tigerio on 2026/3/6.
+//
+
+import Testing
+
+struct tigerflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
